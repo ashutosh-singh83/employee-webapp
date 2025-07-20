47bbb61c383e63ee71fe9a1c216c56cdbdd71ff5
Ashutosh Durvijay Singh <ashutosh_durvijaysingh@epam.com>
2025-07-21 00:27:57 +0530

    Initial commit

---
+++ EmployeeWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+    }
+}
---
+++ EmployeeWebApp/Data/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeWebApp.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeWebApp.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
